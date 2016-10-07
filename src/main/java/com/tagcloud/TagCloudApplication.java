ed0f9c9ace13851f3886aa851eb018f46a71e83b
Marcus Widegren <Marcus Widegren>
2016-10-07 16:46:59 +0200

    Generated an empty spring-boot project

---
+++ src/main/java/com/tagcloud/TagCloudApplication.java
@@ -0,0 +1,12 @@
+package com.tagcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagCloudApplication.class, args);
+	}
+}
